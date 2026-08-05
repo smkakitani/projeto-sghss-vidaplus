863fa60a6e8b7ecb801be3f697ff9cbad21b0e69
Sara <82690252+smkakitani@users.noreply.github.com>
2026-08-04 23:18:29 -0300

    Refatoração de código

--- src/components/GeneralDashboard.jsx
+++ src/components/GeneralDashboard.jsx
@@ -1,13 +1,59 @@
 import { removeAccentPTBR } from "./LocalData";
 import vidaPlusLogo from "../assets/logo_vidaplus_.svg";
-
 // Styles
 import "../styles/GeneralDashboard.css";
 import styled, { keyframes } from "styled-components";
-
 // MUI
 import { Avatar } from "@mui/material";
 import NotificationsSharpIcon from "@mui/icons-material/NotificationsSharp";
+import { NavLink } from "react-router";
+
+// Página geral determinando o grid
+const slideLeft = keyframes`
+  0% {
+    transform: translateX(-1400px);
+    opacity: 0;
+    filter: blur(30px);
+  }
+  100% {
+    transform: translateZ(0);
+    opacity: 1;
+    filter: blur(0);
+  }
+`;
+const DivAnimation = styled.div`
+  animation: ${slideLeft} 1000ms cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+`;
+export function MainGridDashboard({
+  pageName,
+  children,
+  tabs = [],
+  tabsIcon = {},
+  userName,
+  handleTabOnClick,
+}) {
+  return (
+    <DivAnimation id="main-grid" className={pageName}>
+      <nav>
+        <LogoVidaPlus />
+        <menu>
+          {tabs.map((name) => (
+            <ItemTab
+              key={name}
+              tabId={name}
+              tabIcon={tabsIcon[name]}
+              path={name === "sair" ? "/" : name}
+              classNameTab={"button-menu"}
+              onTabClick={handleTabOnClick}
+            />
+          ))}
+        </menu>
+      </nav>
+      <UserProfile userName={userName} />
+      <main>{children}</main>
+    </DivAnimation>
+  );
+}
 
 // Logo da instituição 97px
 const heartbeat = keyframes`
@@ -36,11 +82,7 @@ const LogoAnimation = styled.img`
 export function LogoVidaPlus() {
   return (
     <div id="vp-logo">
-      <LogoAnimation
-        className="logo"
-        src={vidaPlusLogo}
-        alt="logo de VidaPlus"
-      />
+      <LogoAnimation className="logo" src={vidaPlusLogo} alt="logo de VidaPlus" />
       <p>VidaPlus</p>
     </div>
   );
@@ -57,17 +99,23 @@ const ButtonAnimation = styled.button`
     transform-origin: center;
   }
 `;
-export function ItemTab({ tabId, handleMenuTab, classNameTab, tabIcon }) {
+const StyleNavLink = styled(NavLink)`
+  text-decoration: none;
+`;
+export function ItemTab({ tabId, onTabClick, classNameTab, tabIcon, path }) {
   return (
     <li className={removeAccentPTBR(tabId)}>
-      <ButtonAnimation
-        onClick={handleMenuTab}
-        type="button"
-        className={classNameTab}
-      >
-        <span>{tabIcon}</span>
-        <span className="hidden-text">{tabId.replaceAll("-", " ")}</span>
-      </ButtonAnimation>
+      <StyleNavLink to={removeAccentPTBR(path)}>
+        <ButtonAnimation
+          onClick={onTabClick}
+          type="button"
+          className={classNameTab}
+          value={tabId}
+        >
+          <span>{tabIcon}</span>
+          <span className="hidden-text">{tabId.replaceAll("-", " ")}</span>
+        </ButtonAnimation>
+      </StyleNavLink>
     </li>
   );
 }
@@ -91,7 +139,7 @@ export function MainContent({ tabId, className, children }) {
   );
 }
 
-export function UserProfile({ userName = "Miau" }) {
+export function UserProfile({ userName = "unkown" }) {
   function getLetter(str) {
     return str.charAt(0).toUpperCase();
   }
@@ -103,29 +151,5 @@ export function UserProfile({ userName = "Miau" }) {
         Olá, <br /> {userName}
       </p>
     </section>
-  );
-}
-
-// Página geral determinando o grid
-const slideLeft = keyframes`
-  0% {
-    transform: translateX(-1400px);
-    opacity: 0;
-    filter: blur(30px);
-  }
-  100% {
-    transform: translateZ(0);
-    opacity: 1;
-    filter: blur(0);
-  }
-`;
-const DivAnimation = styled.div`
-  animation: ${slideLeft} 1000ms cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-`;
-export function MainGridDashboard({ pageName, children }) {
-  return (
-    <DivAnimation id="main-grid" className={pageName}>
-      {children}
-    </DivAnimation>
   );
 }
